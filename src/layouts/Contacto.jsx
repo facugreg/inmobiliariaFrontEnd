ffeb11f467badc931bc203327bcf4023f97ba4db
IriDev <irirepupilli123@gmail.com>
2025-09-10 19:00:49 -0300

    ahora se renderiza contacto

--- src/layouts/Contacto.jsx
+++ src/layouts/Contacto.jsx
@@ -1,13 +1,18 @@
 import { CContainer } from "@coreui/react";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-import { Form } from "react-router-dom";
 import { FormContacto } from "../components/FormContacto.jsx";
 
 export const Contacto = () => {
+  const navItems = [
+    { nombre: "Comprar", direccion: "/comprar" },
+    { nombre: "Alquilar", direccion: "/alquilar" },
+    { nombre: "Contacto", direccion: "/contacto" },
+  ]
   return (
+    <>
   <CContainer fluid className="p-0">
-    <Header />
+    <Header navItems={navItems} />
     <CContainer className="d-flex justify-content-center align-items-center p-5" style={{ minHeight: '80px' }}> 
     <h1>Contactate con nosotros</h1>
     </CContainer>
@@ -16,5 +21,6 @@ export const Contacto = () => {
     </CContainer>
     <Footer />
   </CContainer>  
+    </>
   );
 }
