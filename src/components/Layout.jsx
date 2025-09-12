db0db6cdcfa863fafcc19e2e451b6724e6e6ec11
Irina Repupilli <irirepupilli123@gmail.com>
2025-09-12 16:07:11 -0300

    Componente layput
    Se renderiza una vez en la app y el outlet renderiza la pag necesaria

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx'; // Asumiendo que existe; si no, créalo similar al tuyo
+import { CContainer } from '@coreui/react';
+
+export default function Layout({ userType, setUserType, onLogout }) {
+  return (
+    <CContainer fluid className="p-0 min-vh-100 d-flex flex-column">
+      <Header userType={userType} setUserType={setUserType} onLogout={onLogout} />
+      <main className="flex-grow-1">
+        <Outlet /> {/* Aquí se renderiza el contenido de cada ruta */}
+      </main>
+      <Footer />
+    </CContainer>
+  );
+}
