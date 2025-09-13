4888ab7a7ff2cbbced3bf8ec8cb018ca9da34805
Irina Repupilli <irirepupilli123@gmail.com>
2025-09-13 20:47:03 -0300

    Centrado del texto encabezado

--- src/components/partsLists/EncabezadoLista.jsx
+++ src/components/partsLists/EncabezadoLista.jsx
@@ -5,9 +5,9 @@ export function EncabezadoLista({columns}){
   return(
     <>
     <CContainer className="mt-3 border-top border-gray-300 rounded-2">
-      <CRow className= 'mx-1 fw-bold text-left align-items-center'>
+      <CRow className= 'fw-bold text-left align-items-center'>
         {columns.map((col, index) => (
-          <CCol key={index} lg={col.size || 3}>
+          <CCol key={index} lg={col.size || 3} className="mt-2" >
             {col.key.toUpperCase() || 'N/A'}
           </CCol>
         ))
