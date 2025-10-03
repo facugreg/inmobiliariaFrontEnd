c1f114070450683ad0ea4d120b253679ba3ff580
facugreg <gregoretfacu18@gmail.com>
2025-10-03 17:54:16 -0300

    filtro por localidad para inmuebles

--- src/pages/admin/inmueble/getInmuebles.jsx
+++ src/pages/admin/inmueble/getInmuebles.jsx
@@ -1,22 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const getInmuebles = async (tipoInmueble, query) => {
+const getInmuebles = async (tipoInmueble, localidad, query) => {
   const PATH = 'http://localhost:3000/api/inmuebles';
   const response = await axios.get(PATH, {
-  params: { tipo: tipoInmueble, calle: query } });
-  return response.data.data; 
+    params: { tipo: tipoInmueble, calle: query, localidad: localidad },
+  });
+  return response.data.data;
 };
 
-function useInmuebles({tipoInmueble, query}) {
+function useInmuebles({ tipoInmueble, localidad, query }) {
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["inmuebles", tipoInmueble, query],
-    queryFn: () => getInmuebles(tipoInmueble, query),
+    queryKey: ['inmuebles', tipoInmueble, localidad, query],
+    queryFn: () => getInmuebles(tipoInmueble, localidad, query),
     refetchOnWindowFocus: false,
   });
 
   return {
-    inmuebles: data || [], // devuelvo este array vacío si no hay data 
+    inmuebles: data || [], // devuelvo este array vacío si no hay data
     isError,
     error,
     isLoading,
