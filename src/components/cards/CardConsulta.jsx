a757527ee73a00ecbda4c070ff7d651cdcc57923
Irina Repupilli <irirepupilli123@gmail.com>
2025-10-08 13:49:20 -0300

    Card para mostrar la consulta

--- src/components/cards/CardConsulta.jsx
+++ src/components/cards/CardConsulta.jsx
@@ -1,19 +1,18 @@
 import { CIcon } from '@coreui/icons-react';
 import { cilUser } from '@coreui/icons';
 import { CCard, CCardBody } from '@coreui/react';
-export function Resenas ( {props} ){
-    const { nombreUsuario, descripcionResena, ResenaOConsulta= "Reseña" } = props;
+export function CardConsulta ( {props} ){
+    // const { nombreUsuario, descripcionResena, ResenaOConsulta= "Reseña" } = props;
     // agregué resenaOConsulta para que en cada resena haya que indicar si es una resena o una consulta general
     return(
     <>
-    <h4 className="mb-3">{ResenaOConsulta}</h4>
     <CCard className="mb-3">
         <CCardBody>
         <div className="d-flex align-items-center mb-2">
-            <CIcon icon={cilUser} className="me-2" />
-            <strong>{nombreUsuario}</strong>
+            <CIcon size={'lg'} icon={cilUser} className="me-2 text-primary" />
+            <strong>{props.usuario.nombre} {props.usuario.apellido}</strong>
         </div>
-        <p>{descripcionResena}</p>
+        <p>{props.descripcion}</p>
         </CCardBody>
     </CCard>
     </>
